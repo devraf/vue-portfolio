6f2617cd948e60ce74e06b4fbccc4a60fff88a76
devraf <rafael.perales@outlook.com>
2019-08-01 02:53:31 -0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app">
+    <Hero />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Hero from "./components/Hero.vue";
+import Footer from "@/components/Footer.vue";
+
+export default {
+  name: "app",
+  components: {
+    Hero,
+    Footer
+  }
+};
+</script>
+
+<style>
+@import url("https://use.typekit.net/vot4lml.css");
+
+*,
+*::after,
+*::before {
+  margin: 0;
+  padding: 0;
+  box-sizing: inherit;
+}
+html {
+  font-size: 62.5%;
+}
+body {
+  font-family: program, sans-serif;
+  font-weight: 400;
+  font-style: normal;
+
+  font-size: 1.2rem;
+  line-height: 1.7;
+  color: hsla(0, 0%, 1%, 1);
+  box-sizing: border-box;
+  height: 100vh;
+}
+/* .component-fade-enter-active,
+.component-fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+.component-fade-enter, .component-fade-leave-to {
+  opacity: 0;
+} */
+</style>
---
+++ src/components/Hero.vue
@@ -0,0 +1,45 @@
+<template>
+  <section class="hero">
+    <div>
+      <p class="hero-text">{{ name }}</p>
+      <svg width="303" height="9">
+        <rect x="0" y="0" width="297" height="3" fill="hsla(0, 100%, 0%, 1)" />
+      </svg>
+      <p class="hero-text">{{ job }}</p>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Hero",
+  data() {
+    return {
+      name: "Rafael Perales",
+      job: "Front-End Developer"
+    };
+  }
+};
+</script>
+
+<style>
+.hero {
+  display: grid;
+  grid-template-columns: repeat(1, 1fr);
+  height: 80vh;
+}
+
+.hero-text {
+  text-align: center;
+  font-size: 3.5rem;
+}
+
+.hero div {
+  align-self: center;
+  justify-self: center;
+}
+
+.hero svg {
+  padding: 0.3rem;
+}
+</style>
